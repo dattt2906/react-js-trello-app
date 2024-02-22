b30c41dac8918852bc43b376b04941ec76f3f993
dattt2906 <dattt@rubicontechno.com>
2024-02-22 10:57:19 +0700

    layer react

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div className="trello-master">
-      <nav className="navbar-app">App bar</nav>
+      <nav className="navbar-app">Trello Clone</nav>
       <nav className="navbar-board">Board bar</nav>
       <div className="board-columns">
         <div className="column">
@@ -13,8 +13,47 @@ function App() {
               <img className="photo" src="https://img.meta.com.vn/Data/image/2021/07/29/sinh-nhat-doraemon-12.jpg"></img>
               Design and Research
             </li>
+            
+            <li>Third</li>
+            <li>Fourth</li>
             <li>second</li>
             <li>Third</li>
+            <li>Fourth</li>
+            
+          </ul>
+          <footer>Add another card </footer>
+        </div>
+        <div className="column">
+        <header>brainstorm</header>
+          <ul>
+            <li>
+              <img className="photo" src="https://img.meta.com.vn/Data/image/2021/07/29/sinh-nhat-doraemon-12.jpg"></img>
+              Design and Research
+            </li>
+            
+            <li>Third</li>
+            <li>Fourth</li>
+            <li>second</li>
+            <li>Third</li>
+            <li>Fourth</li>
+            
+          </ul>
+          <footer>Add another card </footer>
+        </div>
+        <div className="column">
+        <header>brainstorm</header>
+          <ul>
+            <li>
+              <img className="photo" src="https://img.meta.com.vn/Data/image/2021/07/29/sinh-nhat-doraemon-12.jpg"></img>
+              Design and Research
+            </li>
+            
+            <li>Third</li>
+            <li>Fourth</li>
+            <li>second</li>
+            <li>Third</li>
+            <li>Fourth</li>
+            
           </ul>
           <footer>Add another card </footer>
         </div>
@@ -27,7 +66,49 @@ function App() {
               <img className="photo" src="https://img.meta.com.vn/Data/image/2021/07/29/sinh-nhat-doraemon-12.jpg"></img>
               Design and Research
             </li>
+            
+            <li>Third</li>
+            <li>Fourth</li>
             <li>second</li>
+            <li>Third</li>
+            <li>Fourth</li>
+            
+          </ul>
+          <footer>Add another card </footer>
+        </div>
+        <div className="column">
+        <header>brainstorm</header>
+          <ul>
+            <li>
+              <img className="photo" src="https://img.meta.com.vn/Data/image/2021/07/29/sinh-nhat-doraemon-12.jpg"></img>
+              Design and Research
+            </li>
+            
+            <li>Third</li>
+            <li>Fourth</li>
+            <li>second</li>
+            <li>Third</li>
+            <li>Fourth</li>
+            
+          </ul>
+          <footer>Add another card </footer>
+        </div>
+       
+       
+        <div className="column">
+        <header>brainstorm</header>
+          <ul>
+            <li>
+              <img className="photo" src="https://img.meta.com.vn/Data/image/2021/07/29/sinh-nhat-doraemon-12.jpg"></img>
+              Design and Research
+            </li>
+            
+            <li>Third</li>
+            <li>Fourth</li>
+            <li>second</li>
+            <li>Third</li>
+            <li>Fourth</li>
+            
           </ul>
           <footer>Add another card </footer>
         </div>
